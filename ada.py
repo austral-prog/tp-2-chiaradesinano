3a54f292bbda4f75fd44b5a09fae26f25b89c0c6
chiaradesinano <cdesinano@mail.austral.edu.ar>
2025-03-31 12:30:29 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,11 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name = f"{first_name.lower()} {last_name.lower()}"
+    full_name2 = f"{first_name.title()} {last_name.title()}"
+    full_name3 = f"{first_name.upper()} {last_name.upper()}"
+    full_name4 = f"\t {full_name.lower()}"
+    print(full_name)
+    print(full_name2)
+    print(full_name3)
+    print(full_name4)
