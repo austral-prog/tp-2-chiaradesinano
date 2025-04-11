3d0b767c5734a68b1610eb33526ea560e2fb30f9
chiaradesinano <cdesinano@mail.austral.edu.ar>
2025-04-11 10:26:50 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -3,3 +3,4 @@ def earth():
     y = "Barbados"
     print(f" The result of {x} comes first in the dictionary than {y} is {x<y}")#True
     print(f" The result of {y} comes first in the dictionary than {x} is {x>y}")#False
+
